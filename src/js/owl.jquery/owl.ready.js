3c15abaa06fb90f958aa1266fc7a8b3098a23b26
caezar47 <iss47@ya.ru>
2018-03-12 22:48:45 +0300

    add form block

--- src/js/owl.jquery/owl.ready.js
+++ src/js/owl.jquery/owl.ready.js
@@ -37,4 +37,24 @@ $(function() {
 			}
 		}
 	});
+	$('.owl-partners').owlCarousel({
+		//navText: ['<svg class="icon-svg icon-owl-prev" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#owl-prev"></use></svg>', '<svg class="icon-svg icon-owl-next" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#owl-next"></use></svg> '],
+		navText: ['<svg class="icon-svg icon-carousel-prev" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#carousel-prev"></use></svg>', '<svg class="icon-svg icon-carousel-next" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/img/svg/sprite.svg#carousel-next"></use></svg> '],
+		nav: true,
+		dots: false,
+		mouseDrag: false,
+		items:4,
+		loop:true,
+		margin:15,
+		autoplay:true,
+		autoplayTimeout: 100,
+		autoplaySpeed: 1000,
+		responsive : {
+			0 : {
+				items: 1,
+			},
+			768 : {
+			}
+		}
+	});
 });
