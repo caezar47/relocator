50e019866820572ce89ad40aff5014296ccab8c3
caezar47 <iss47@ya.ru>
2018-03-13 16:02:57 +0300

    ready page screen ver.

--- src/block/site/default/contacts/.plugin.js
+++ src/block/site/default/contacts/.plugin.js
@@ -1,16 +1,15 @@
 function initMap() {  
   var map_container_div_id = 'map-google';
   var cont = $('#' + map_container_div_id);  
-  var CMS__TPL_PATH = '/wp-content/themes/azbn7theme';  
   if(cont.length) {    
     var map_data = JSON.parse(cont.attr('data-map') || '{}');    
     var coordMapOfficeOne = map_data.center;
     var zoomMapOfficeOne = map_data.zoom,
-        styleMapOfficeOne = [{"stylers":[{"hue":"#2c3e50"},{"saturation":250}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":50},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]}],
+        //styleMapOfficeOne = [{"stylers":[{"hue":"#2c3e50"},{"saturation":250}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":50},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]}],
         optionsMapOfficeOne = {
           zoom: zoomMapOfficeOne, 
           center: new google.maps.LatLng(coordMapOfficeOne[0], coordMapOfficeOne[1]), 
-          styles: styleMapOfficeOne
+          //styles: styleMapOfficeOne
         }, 
       idOfficeOne = document.getElementById(map_container_div_id),
       mapOfficeOne = new google.maps.Map(idOfficeOne, optionsMapOfficeOne), 
@@ -19,7 +18,7 @@ function initMap() {
       iconOfficeOne = {   
         //path: "M25,0C12.1,0.5,1.4,10.5,0.1,23.4C-0.1,26,0,28.5,0.5,30.9l0,0c0,0,0,0.3,0.2,0.8c0.4,1.8,1,3.5,1.7,5.1 C5,43,11,53.3,24.5,64.5c0.8,0.7,2,0.7,2.9,0C40.9,53.3,46.9,43,49.5,36.8c0.8-1.6,1.3-3.3,1.7-5.1c0.1-0.5,0.2-0.8,0.2-0.8l0,0 c0.3-1.6,0.5-3.2,0.5-4.9C51.9,11.3,39.7-0.5,25,0z M25.9,38.8c-7,0-12.6-5.6-12.6-12.6S19,13.6,25.9,13.6s12.6,5.6,12.6,12.6     S32.9,38.8,25.9,38.8z", 
         path: "M24,64c0,0,23.8-27,23.8-40.2C47.8,10.7,37.2,0,24,0S0.2,10.7,0.2,23.8S24,64,24,64z M24,7.4 c9.1,0,16.5,7.4,16.5,16.5S33.1,40.3,24,40.3c-9.1,0-16.5-7.4-16.5-16.5S14.9,7.4,24,7.4z", 
-        fillColor: '#0a4587',
+        fillColor: '#ee5f5b',
         strokeColor: '#000000',
         fillOpacity: 1,
         //anchor: new google.maps.Point(26,65),
